94a5b3966fbc915346d4a967bdc72d4d7854d24b
KaoruBB <kaorugmbl1234@gmail.com>
2022-12-03 15:03:04 +0900

    add showtable

--- src/KaoruOriginal.jl
+++ src/KaoruOriginal.jl
@@ -1,5 +1,29 @@
 module KaoruOriginal
-
 # Write your package code here.
+
+export showtable, datascience
+
+using DataFrames
+
+function showtable(df)
+    DataFrame([[names(df)];
+		   [eltype.(eachcol(df))];
+		   collect.(eachrow(df))], [:column; :dtypes; Symbol.(axes(df, 1))])
+end
+
+Base.display(df::AbstractDataFrame) = show(
+	df, header_crayon = DataFrames.crayon"light_green"
+	, subheader_crayon = DataFrames.crayon"white"
+)
+
+module datascience
+export  kaoruoriginalhello
+
+function kaoruoriginalhello(x)
+    println("Hello, $x")
+end
+
+end
+
 
 end
