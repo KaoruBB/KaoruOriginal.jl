4a9a5d2f2c658d37fe588093a92a44d35171e039
KaoruBB <kaorugmbl1234@gmail.com>
2023-01-06 16:23:11 +0900

    wip

--- src/KaoruOriginal.jl
+++ src/KaoruOriginal.jl
@@ -1,7 +1,7 @@
 module KaoruOriginal
 # Write your package code here.
 
-    export showtable, makevisible, datascience, custom_sort, show_html_df
+    export showtable, makevisible, for_plotting, custom_sort, show_html_df
 
     using DataFrames
     using Crayons
@@ -46,27 +46,56 @@ module KaoruOriginal
         end
     end
 
-    # function change_col_colors(; kwargs...)
-    #     print(kwargs)
-    #     prinln(typeof(kwargs))
-    #     if not "header_crayon" in kwargs
-    #         kwargs.append(( "header_crayon" => :white )
-    #     end
-    #     if "subheader_crayon" is not in kwargs
-    #         kwargs.append(( "subheader_crayon" => :white ))
-    #     end
-    #     Base.display(df::AbstractDataFrame) = show(
-    #         df,
-    #         kwargs...
-    #     )
-    # end
 
-    module datascience
+    module for_plotting
         export  kaoruoriginalhello
 
     function kaoruoriginalhello(x)
             println("Hello, $x")
     end
+
+    """
+        hover_info(df::AbstractDataFrame, pair_list::Vector{Pair{Symbol, String}})
+
+    Return a vector of hover text.
+
+
+    # Examples
+
+    ```jldoctest
+    julia> df = DataFrame(
+        index=1:4,
+        name=["John", "Sally", "Kirk", "John"],
+        apple=[2, 4, 5, 6],
+        banana=[3,5,2,0]
+    )
+
+
+    julia> symbols_labels = [
+            :name => "名前",
+            :apple => "りんご",
+            :banana => "バナナ"
+        ]
+    3-element Vector{Pair{Symbol, String}}:
+    :name => "名前"
+    :apple => "りんご"
+    :banana => "バナナ"
+
+
+    julia> hover_info(df, symbols_labels)
+    4-element Vector{String}:
+    "名前: John<br>りんご: 2<br>バナナ: 3"
+    "名前: Sally<br>りんご: 4<br>バナナ: 5"
+    "名前: Kirk<br>りんご: 5<br>バナナ: 2"
+    "名前: John<br>りんご: 6<br>バナナ: 0"
+    ```
+    """
+    function hover_info(df, pair_list)
+        return [
+            join([name * ": " * string(df[i, col]) for (col, name) in pair_list], "<br>") for i in 1:nrow(df)
+        ]
+    end
+
 
 end
 
